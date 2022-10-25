90353aa4845fa3577c6c21a13b195ddc6c2b7b64
七年觐汐 <1609762425@qq.com>
2022-10-25 16:08:21 +0800

    🐳 feat: 新增禁用日期和改变日期的功能

--- index/index.js
+++ index/index.js
@@ -12,18 +12,38 @@ Page({
       y2023m5d10: 'spot',
     },
     disabledDate({ day, month, year }) {
-      console.log(day, month, year, '禁用日期');
       // 例子，今天之后的日期不能被选中
       const now = new Date();
       const date = new Date(year, month - 1, day);
       return date > now;
     },
+    // 需要改变日期时所使用的字段
     changeTime: '',
+    // 存储已经获取过的日期
+    dateListMap: [],
   },
-  onLoad() {},
+  // 获取日期数据，通常用来请求后台接口获取数据
   getDateList({ detail }) {
-    console.log(detail, 'getDateList detail');
+    // 检查是否已经获取过该月的数据
+    if (this.filterGetList(detail)) {
+      // 获取数据
+      console.log(detail, '获取数据');
+    }
   },
+  // 过滤重复月份请求的方法
+  filterGetList({ setYear, setMonth }) {
+    const dateListMap = new Set(this.data.dateListMap);
+    const key = `y${setYear}m${setMonth}`;
+    if (dateListMap.has(key)) {
+      return false;
+    }
+    dateListMap.add(key);
+    this.setData({
+      dateListMap: [...dateListMap]
+    })
+    return true;
+  },
+  // 日期改变的回调
   selectDay({ detail }) {
     console.log(detail, 'selectDay detail');
   },
